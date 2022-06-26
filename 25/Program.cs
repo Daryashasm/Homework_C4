470f660b95c046541536f18ef3c9063acea7fe03
Daria <darena-smol@yandex.ru>
2022-06-26 19:10:56 +0500

    25

---
+++ 25/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B\
+
+Console.Write("Введите число; A = ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число степени; В = ");
+int B = Convert.ToInt32(Console.ReadLine());
+int AstepB = A;
+for (int i = 1; i < B; i++)
+{
+    AstepB = AstepB*A;
+}
+Console.WriteLine($"Число {A}^{B} = {AstepB}");
